b94df0413784c4476c0ba72b8c6f892b79ed7e3c
lslr6302 <mengqing010@gmail.com>
2026-07-26 11:09:21 -0400

    label added lol

--- minigames/start_game/start_target.gd
+++ minigames/start_game/start_target.gd
@@ -6,12 +6,14 @@ signal tutorial_complete
 @export var cylinder_path: NodePath = "Cylinder"
 @export var start_ball_path: NodePath = "StartBall"
 @export var instructions_label_path: NodePath = "Label3D"
+@export var fire_label_path: NodePath = "Label3D2"
 
 @onready var ball: Node = get_node(ball_path)
 @onready var cube: Node = get_node(cube_path)
 @onready var cylinder: Node = get_node(cylinder_path)
 @onready var start_ball: Node = get_node(start_ball_path)
 @onready var instructions_label: Node = get_node_or_null(instructions_label_path)
+@onready var fire_label: Node = get_node_or_null(fire_label_path)
 
 
 func _ready() -> void:
@@ -28,5 +30,8 @@ func _on_start_ball_hit() -> void:
 
 	if instructions_label:
 		instructions_label.hide()
+	
+	if fire_label:
+		fire_label.hide()
 
 	emit_signal("tutorial_complete")
